80dd2653c4b7e74d64d11a27b9d34c913f61eaa2
Lukasz Kaszczyszyn <lukasz.kaszczyszyn@gmail.com>
2025-06-30 21:00:10 +0200

    parameter constructor used

--- Main.java
+++ Main.java
@@ -8,13 +8,14 @@ public class Main {
     book1.setTitle("Hobbit");
     book1.setAuthor("J.R.R. Tolkien");
 
-
     book2.setTitle("Harry Potter and the Philosopher's Stone");
     book2.setAuthor("J.K. Rowling");
 
     book1.printBook();
     book2.printBook();
 
+    Book book = new Book("Sherlock Holmes", "Sir Arthur Conan Doyle", 1854);
+    book.printBook();
   }
 
 
